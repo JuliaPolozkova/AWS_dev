4cd4c9823533326f1c2370f1a5e23a5141857928
Julia_Polozkova <Jpolozkova@copyright.com>
2021-06-04 07:15:16 -0400

    Test app

---
+++ OnlineShop/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShop.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ProductController : ControllerBase
+	{
+		private static readonly List<string> Products = new List<string>
+		{
+			"Boots", "Dress", "Bikini", "Hat", "Bag", "Hoodie"
+		};
+
+		private readonly ILogger<ProductController> _logger;
+
+		public ProductController(ILogger<ProductController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<Product> Get()
+		{
+			int i = -1;
+			return Products.Select(index => new Product
+			{
+				Id = i++,
+				Name = Products[i]
+			})
+			.ToArray();
+		}
+
+		[HttpPut]
+		public IEnumerable<Product> Add(string name)
+		{
+			Products.Add(name);
+			int i = -1;
+			return Products.Select(index => new Product
+			{
+				Id = i++,
+				Name = Products[i]
+			})
+			.ToArray();
+		}
+	}
+}
---
+++ OnlineShop/Product.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineShop
+{
+	public class Product
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+	}
+}
